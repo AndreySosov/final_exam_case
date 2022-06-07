cb804d0aff9be829230985375d4616eb80c15797
AndreySosov <77112597+AndreySosov@users.noreply.github.com>
2022-06-07 21:00:28 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -29,7 +29,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
